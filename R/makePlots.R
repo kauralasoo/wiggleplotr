8845ceead11c721ff8c874629435e7c159c3dcd4
Kaur Alasoo <kaur.alasoo@gmail.com>
2016-06-21 16:15:08 +0100

    Remove background grid + fix alignment of subplots
    Use cowplot::plot_grid to align different subplots
    Switch to theme_light() and remove background grid.
    Add chromosome name to x axis label.
    Add axis ticks and labels for the coverage plot.

--- R/makePlots.R
+++ R/makePlots.R
@@ -1,4 +1,5 @@
-plotTranscriptStructure <- function(exons_df, limits = NA, connect_exons = TRUE){
+plotTranscriptStructure <- function(exons_df, limits = NA, connect_exons = TRUE, 
+                                    xlabel = "Distance from gene start (bp)"){
   
   #Extract the position for plotting transcript name
   transcript_annot = dplyr::group_by(exons_df, transcript_id) %>% 
@@ -13,13 +14,17 @@ plotTranscriptStructure <- function(exons_df, limits = NA, connect_exons = TRUE)
   }
   plot = plot + geom_rect(aes(xmin = start, xmax = end, ymax = transcript_rank + 0.2, ymin = transcript_rank - 0.2, fill = feature_type)) + 
     geom_text(aes(x = start, y = transcript_rank + 0.25, label = transcript_label), data = transcript_annot, hjust = 0, vjust = 0, size  =4) +
+    theme_light() +
     theme(plot.margin=unit(c(0,1,1,1),"line"), 
           axis.title.y = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank(),
           legend.position="none",
-          panel.background = element_blank()) +
-    xlab("Distance from gene start (bp)") +
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text.y = element_text(colour = "grey10"),
+          strip.background = element_rect(fill = "grey85")) +
+    xlab(xlabel) +
     facet_grid(type~.) +
     scale_y_continuous(expand = c(0,0.5)) +
     scale_fill_manual(values = c("#2c7bb6","#abd9e9")) + 
@@ -35,17 +40,19 @@ makeCoveragePlot <- function(coverage_df, limits, alpha, fill_palette){
   coverage_plot = ggplot(coverage_df, aes(bins, coverage, group = sample_id, fill = colour_group)) + 
     geom_area(alpha = alpha, position = "identity") + 
     facet_grid(track_id~.) +
+    theme_light() +
     theme(axis.text.x = element_blank(), 
           axis.title.x = element_blank(), 
           axis.ticks.x = element_blank(),
           plot.margin=unit(c(1,1,0,1),"line"),
-          axis.title.y = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank(),
           legend.position="none",
-          panel.background = element_blank()) +
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text.y = element_text(colour = "grey10"),
+          strip.background = element_rect(fill = "grey85")) +
     scale_x_continuous(limits = limits, expand = c(0,0)) +
     scale_y_continuous(expand = c(0,0)) +
-    scale_fill_manual(values = fill_palette)
+    scale_fill_manual(values = fill_palette) +
+    ylab("FPM")
   return(coverage_plot)
 }
--- R/wiggleplotr-package.r
+++ R/wiggleplotr-package.r
@@ -3,11 +3,11 @@
 #' @name wiggleplotr
 #' @docType package
 #' @import GenomicRanges
-#' @import GenomicFeatures
 #' @import IRanges
 #' @import ggplot2
 #' @import rtracklayer
 #' @import grid
+#' @import plyr
 #' @import dplyr
-#' @import gridExtra
+#' @import cowplot
 NULL
