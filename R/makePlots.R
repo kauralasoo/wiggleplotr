6c0d7bac7ca67849c5174600742fd26dc3896bf3
bioguo <guokai8@gmail.com>
2019-11-24 10:45:09 -0600

    Update makePlots.R

--- R/makePlots.R
+++ R/makePlots.R
@@ -1,6 +1,6 @@
 plotTranscriptStructure <- function(exons_df, limits = NA, connect_exons = TRUE,  
                                     xlabel = "Distance from gene start (bp)", 
-                                    transcript_label = TRUE,RNA=FALSE){
+                                    transcript_label = TRUE){
   
   #Extract the position for plotting transcript name
   transcript_annot = dplyr::group_by_(exons_df, ~transcript_id) %>% 
@@ -48,7 +48,7 @@ plotTranscriptStructure <- function(exons_df, limits = NA, connect_exons = TRUE,
 }
 
 makeCoveragePlot <- function(coverage_df, limits, alpha, fill_palette, coverage_type,
-                             show_group=TRUE,legend_position="none",ylim=NULL){
+                             show_group=TRUE,legend_position="none",ylim=NULL,,RNA=FALSE){
   #Plot coverage over a region
   coverage_plot = ggplot(coverage_df, aes_(~bins, ~coverage, group = ~sample_id, alpha = ~alpha)) + 
     geom_blank() +
